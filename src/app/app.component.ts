22c0de190ae6e153e8de697a93bc8a5e51317c81
BwanaQ <thunjawax@gmail.com>
2021-04-18 15:22:45 +0300

    recap to pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { Goal } from './goal';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -6,35 +5,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  goals: Goal[] = [
-    {
-      id: 1,
-      name: 'Watch finding Nemo',
-      description: 'Find an online version and watch merlin find his son',
-    },
-    {
-      id: 2,
-      name: 'Buy Cookies',
-      description: 'I have to buy cookies for the parrot',
-    },
-    {
-      id: 3,
-      name: 'Get new Phone Case',
-      description: 'Diana has her birthday coming up soon',
-    },
-    {
-      id: 4,
-      name: 'Get Dog Food',
-      description: 'Pupper likes expensive sancks',
-    },
-    { id: 5, name: 'Solve math homework', description: 'Damn Math' },
-    {
-      id: 6,
-      name: 'Plot my world domination plan',
-      description: 'Cause I am an evil overlord',
-    },
-  ];
-  title = 'goals';
-  goal = 'recap angular';
   constructor() {}
 }
--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Goal } from './../goal';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-goal-detail',
   templateUrl: './goal-detail.component.html',
-  styleUrls: ['./goal-detail.component.css']
+  styleUrls: ['./goal-detail.component.css'],
 })
 export class GoalDetailComponent implements OnInit {
+  @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  goalComplete(complete: boolean) {
+    this.isComplete.emit(complete);
   }
+  constructor() {}
 
+  ngOnInit() {}
 }
